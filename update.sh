d33ba99b154b404ae13be65322e1d018812cb82c
David Viteri <davidteri91@gmail.com>
2016-09-16 12:02:36 +0200

    scripts: update sony-common with proper commit

--- update.sh
+++ update.sh
@@ -52,6 +52,7 @@ then
   git checkout -b master
   git branch -D n
   git checkout -b n
+  git fetch https://review.sonyaosp.org/SonyAosp/device_sony_common refs/changes/99/299/1 && git cherry-pick FETCH_HEAD
   git push -f origin n
 else
   echo 'no updates my friends!'
@@ -66,6 +67,7 @@ then
   git checkout -b m-mr1
   git branch -D mm-6.0
   git checkout -b mm-6.0
+  git fetch https://review.sonyaosp.org/SonyAosp/device_sony_common refs/changes/99/299/1 && git cherry-pick FETCH_HEAD
   git push -f origin mm-6.0
 else
   echo 'no updates my friends!'
