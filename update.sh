f374d3452d0328447ac7c32f125127177484c442
David <Davidteri91@gmail.com>
2016-12-24 12:13:26 +0100

    scripts: welcome 8974 to separate cherry-pick again
    Signed-off-by: David Viteri <davidteri91@gmail.com>

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@
 git config --global user.name "Erik Castricum"
 git config --global user.email "erikcas1972@gmail.com"
 
-for DEVICE in common sepolicy rhine amami honami togari shinano castor castor_windy sirius aries leo scorpion scorpion_windy kanuti tulip kitakami ivy karin karin_windy satsuki sumire suzuran loire kugo suzu tone dora kagura
+for DEVICE in common sepolicy amami honami togari castor castor_windy sirius aries leo scorpion scorpion_windy kanuti tulip kitakami ivy karin karin_windy satsuki sumire suzuran loire kugo suzu tone dora kagura
 do
 cd device_sony_$DEVICE
 git reset --hard
@@ -52,6 +52,102 @@ else
 fi
 cd ../
 done
+
+cd device_sony_rhine
+git reset --hard
+git fetch sony
+if git checkout m-mr1 &&
+    git fetch sony m-mr1 &&
+    [ `git rev-list HEAD...sony/m-mr1 --count` != 0 ]
+then
+  echo 'device update(s) detected! Lets pull changes!'
+  git checkout sony/m-mr1
+  git branch -D m-mr1
+  git checkout -b m-mr1
+  git branch -D android-6.0
+  git checkout -b android-6.0
+  git push -f origin android-6.0
+else
+  echo 'no updates my friends!'
+fi
+if git checkout n-mr0 &&
+    git fetch sony n-mr0 &&
+    [ `git rev-list HEAD...sony/n-mr0 --count` != 0 ]
+then
+  echo 'device update(s) detected! Lets pull changes!'
+  git checkout sony/n-mr0
+  git branch -D n-mr0
+  git checkout -b n-mr0
+  git branch -D android-7.0
+  git checkout -b android-7.0
+  git push -f origin android-7.0
+else
+  echo 'no updates my friends!'
+fi
+if git checkout master &&
+    git fetch sony master &&
+    [ `git rev-list HEAD...sony/master --count` != 0 ]
+then
+  echo 'device update(s) detected! Lets pull changes!'
+  git checkout sony/master
+  git branch -D master
+  git checkout -b master
+  git branch -D android-7.1
+  git checkout -b android-7.1
+  git fetch https://review.sonyaosp.org/SonyAosp/device_sony_rhine refs/changes/37/437/1 && git cherry-pick FETCH_HEAD
+  git push -f origin android-7.1
+else
+  echo 'no updates my friends!'
+fi
+cd ../
+
+cd device_sony_shinano
+git reset --hard
+git fetch sony
+if git checkout m-mr1 &&
+    git fetch sony m-mr1 &&
+    [ `git rev-list HEAD...sony/m-mr1 --count` != 0 ]
+then
+  echo 'device update(s) detected! Lets pull changes!'
+  git checkout sony/m-mr1
+  git branch -D m-mr1
+  git checkout -b m-mr1
+  git branch -D android-6.0
+  git checkout -b android-6.0
+  git push -f origin android-6.0
+else
+  echo 'no updates my friends!'
+fi
+if git checkout n-mr0 &&
+    git fetch sony n-mr0 &&
+    [ `git rev-list HEAD...sony/n-mr0 --count` != 0 ]
+then
+  echo 'device update(s) detected! Lets pull changes!'
+  git checkout sony/n-mr0
+  git branch -D n-mr0
+  git checkout -b n-mr0
+  git branch -D android-7.0
+  git checkout -b android-7.0
+  git push -f origin android-7.0
+else
+  echo 'no updates my friends!'
+fi
+if git checkout master &&
+    git fetch sony master &&
+    [ `git rev-list HEAD...sony/master --count` != 0 ]
+then
+  echo 'device update(s) detected! Lets pull changes!'
+  git checkout sony/master
+  git branch -D master
+  git checkout -b master
+  git branch -D android-7.1
+  git checkout -b android-7.1
+  git fetch https://review.sonyaosp.org/SonyAosp/device_sony_shinano refs/changes/36/436/1 && git cherry-pick FETCH_HEAD
+  git push -f origin android-7.1
+else
+  echo 'no updates my friends!'
+fi
+cd ../
 
 cd kernel
 git reset --hard
